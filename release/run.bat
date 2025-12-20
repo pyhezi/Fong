dde1dd965e446a4b52952723e08a560188a44493
pyhezi <pyhezi@gmail.com>
2025-12-21 06:14:24 +0800

    503

--- release/run.bat
+++ release/run.bat
@@ -1,16 +1,8 @@
-move .\leanbackJavaArm64_v8a\release\*.apk .\
-move .\leanbackJavaArmeabi_v7a\release\*.apk .\
-move .\leanbackPythonArm64_v8a\release\*.apk .\
-move .\leanbackPythonArmeabi_v7a\release\*.apk .\
-move .\mobileJavaArm64_v8a\release\*.apk .\
-move .\mobileJavaArmeabi_v7a\release\*.apk .\
-move .\mobilePythonArm64_v8a\release\*.apk .\
-move .\mobilePythonArmeabi_v7a\release\*.apk .\
-rmdir /s /q .\leanbackJavaArm64_v8a
-rmdir /s /q .\leanbackJavaArmeabi_v7a
-rmdir /s /q .\leanbackPythonArm64_v8a
-rmdir /s /q .\leanbackPythonArmeabi_v7a
-rmdir /s /q .\mobileJavaArm64_v8a
-rmdir /s /q .\mobileJavaArmeabi_v7a
-rmdir /s /q .\mobilePythonArm64_v8a
-rmdir /s /q .\mobilePythonArmeabi_v7a
+move .\leanbackArm64_v8a\release\*.apk .\
+move .\leanbackArmeabi_v7a\release\*.apk .\
+move .\mobileArm64_v8a\release\*.apk .\
+move .\mobileArmeabi_v7a\release\*.apk .\
+rmdir /s /q .\leanbackArm64_v8a
+rmdir /s /q .\leanbackArmeabi_v7a
+rmdir /s /q .\mobileArm64_v8a
+rmdir /s /q .\mobileArmeabi_v7a
